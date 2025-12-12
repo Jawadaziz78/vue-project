13eca6620e9c887505266abae90929aed21a4a7b
Ubuntu <ubuntu@ip-172-31-77-148.ec2.internal>
2025-12-12 11:21:54 +0000

    fix: update name to Jawad Aziz on development

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
     <div class="col text-center">
       <h1 class="msg">{{ msg }}</h1>
       <p>
-        A Jawad project setup using vue-cli, created to easily scaffold new Vue.js projects doing now on development branch.
+        A Jawad Aziz project setup using vue-cli, created to easily scaffold new Vue.js projects doing now on development
+        branch.
         <br />
         Find out more at
         <a class="custom" href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>
